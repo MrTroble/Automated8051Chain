080e062cb312a3c6a6273e234b074793daed1d16
MrTroble <nifroh56@gmail.com>
2020-03-19 12:17:41 +0100

    Updatet commands

--- build.py
+++ build.py
@@ -8,3 +8,12 @@ for st in fp:
     if not (rtc == 1 or rtc == 0):
         print("Errors found! In compile stange!")
         exit(-1)
+
+rtc = sb.call("git add Test.c")
+if not rtc == 0:
+    print("Error adding files!")
+    exit(-1)
+rtc = sb.call("git commit -m \"Auto versioning\"")
+if not rtc == 0:
+    print("Error commiting files!")
+    exit(-1)
