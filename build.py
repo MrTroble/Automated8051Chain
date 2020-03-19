8af95ea1aa785678a3146c9a1a191ffdcf400eb9
MrTroble <nifroh56@gmail.com>
2020-03-19 12:57:17 +0100

    Added variable project build

--- build.py
+++ build.py
@@ -1,13 +1,20 @@
 import subprocess as sb
+import json
 
+fp = open("settings.json", "r")
+settings = json.load(fp)
+fp.close()
 fp = open("cmds.txt", "r")
 
 for st in fp:
+    for x, y in dict(settings).items():
+        st = st.replace("%" + x + "%", y)
     rtc = sb.call(st, shell=True)
     print("Compile stage:" + str(rtc))
     if not (rtc == 1 or rtc == 0):
         print("Errors found! In compile stange!")
         exit(-1)
+fp.close()
 
 rtc = sb.call("git add Test.c")
 if not rtc == 0:
