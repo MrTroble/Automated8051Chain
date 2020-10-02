2bebda92834ebbd877893fb8029e6bc56fe36822
MrTroble <nifroh56@gmail.com>
2020-10-02 08:53:58 +0200

    Auto versioning

--- Test.c
+++ Test.c
@@ -9,14 +9,41 @@ typedef unsigned int uint32_t;
 #define interrupt(x)
 #endif
 
+#define POR1 1
+#define POR2 2
+#define POR3 4
+#define POR4 8
+#define POR5 16
+#define POR6 32
+#define POR7 64
+#define POR8 128
+
+uint8_t TABLE[8] = {POR1, POR2, POR3, POR4, POR5, POR6, POR7, POR8};
+
+inline void sport(const uint32_t port, const uint8_t input) {
+    if (input) {
+        P4 |= port;
+    } else {
+        P4 &= ~port;
+    }
+}
+inline uint8_t port(const uint32_t port) {
+    return P4 & port;
+}
+
+uint8_t taster[4][3];
+uint8_t i = 0, x = 0;
+
 void main() {
     Grundeinstellungen();
 
-    P4 = 0;
-    P3 = 3;
-
     while (1) {
-        P4 = P3;
-        P1 = P4;
+        for (i = 0; i < 4; i++) {
+            P4 |= 15;
+            sport(TABLE[i], 0);
+            for (x = 0; x < 3; x++) {
+                taster[i][x] = port(TABLE[x + 4]);
+            }
+        }
     }
 }
